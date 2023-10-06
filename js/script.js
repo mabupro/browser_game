a1e4b950ca243ec414c35099938b0ef464ddc096
mabupro <mabupro787@gmail.com>
2023-10-06 17:33:23 +0900

    ft/use chatGPT

--- js/script.js
+++ js/script.js
@@ -1,46 +1,18 @@
+// 1. 初期設定: ゲームに必要な変数やDOM要素を設定します。
 const gameBoard = document.getElementById('game-board');
-const totalTiles = 10; 
+const totalTiles = 10;
 let whiteTiles = [];
 let score = 0;
-let stage = 1; 
-let shouldLevelUp = false;  
-let consecutiveCorrect = 0; 
-let timerId;  
-let displayTime = 5;  
-let selectionTime = 15;  
-let canClick = false;  
+let stage = 1;
+let shouldLevelUp = false;
+let consecutiveCorrect = 0;
+let timerId;
+let displayTime = 5;
+let selectionTime = 15;
+let canClick = false;
 
-// ステージを増やす関数
-function incrementStage() {
-    // レベルアップフラグがtrueならステージを増やす
-    if (shouldLevelUp) {  
-        stage++;
-        document.getElementById('stage').textContent = stage;
-        shouldLevelUp = false;  
-    }
-}
-
-// 「Next」ボタンがクリックされたときの処理
-document.getElementById('next-button').addEventListener('click', function () {
-    incrementStage(); // ステージ増加処理
-    startGame(); // ゲームスタート処理
-    document.getElementById('next-button').disabled = true; // 「Next」ボタンを無効化
-});
-
-// ゲームスタート時のメッセージ非表示
-document.getElementById('start-message').style.display = 'none';
-
-function incrementScore() {
-    score++;
-    document.getElementById('score').textContent = score;
-}
-
-function resetScore() {
-    score = 0;
-    document.getElementById('score').textContent = score;
-}
-
-// タイルを生成
+// 2. ゲームセットアップ: タイルを生成し、ユーザーの入力（キー入力やボタンクリック）に応じてゲームを開始します。
+// 2.1. タイル生成とクリックイベントのセットアップ
 for (let i = 0; i < totalTiles; i++) {
     let tile = document.createElement('div');
     tile.className = 'tile';
@@ -48,49 +20,37 @@ for (let i = 0; i < totalTiles; i++) {
     gameBoard.appendChild(tile);
 }
 
-// ゲームをスタートする関数
+// 2.2. キー入力またはボタンクリックでゲームスタート
+document.getElementById('next-button').addEventListener('click', startGameSequence);
+window.addEventListener('keydown', startGameOnKeyPress);
+
+// 3. ゲームスタート: 白いタイルを表示し、ユーザーに選択を促します。
 function startGame() {
     clearInterval(timerId);
-
     document.getElementById('next-button').disabled = true;
-
-    // 白いタイルのクラスをすべて削除
-    document.querySelectorAll('.tile.white').forEach(tile => {
-        tile.classList.remove('white');
-    });
-
+    document.querySelectorAll('.tile.white').forEach(tile => tile.classList.remove('white'));
     canClick = false;
 
-    let numberOfWhiteTiles = Math.min(3 + Math.floor(stage / 3), 5);  
-
-    // ステージが上がるごとにタイマーを減らす
+    let numberOfWhiteTiles = Math.min(3 + Math.floor(stage / 3), 5);
     displayTime = Math.max(5 - Math.floor(stage / 3), 3);
     selectionTime = Math.max(15 - Math.floor(stage / 3) * 2, 5);
 
     resetScore();
     whiteTiles = [];
 
-    // 白いタイルのインデックスをランダムに生成
-    while (whiteTiles.length < numberOfWhiteTiles) {  
+    while (whiteTiles.length < numberOfWhiteTiles) {
         let randomIndex = Math.floor(Math.random() * totalTiles);
         if (!whiteTiles.includes(randomIndex)) {
             whiteTiles.push(randomIndex);
         }
     }
 
-    // 白いタイルを一定時間表示
-    whiteTiles.forEach(index => {
-        gameBoard.children[index].classList.add('white');
-    });
+    whiteTiles.forEach(index => gameBoard.children[index].classList.add('white'));
 
-    // [displayTime]秒後に白いタイルを非表示にし、スコアをリセット
     setTimeout(() => {
-        document.querySelectorAll('.tile.white').forEach(tile => {
-            tile.classList.remove('white');
-        });
+        document.querySelectorAll('.tile.white').forEach(tile => tile.classList.remove('white'));
         resetScore();
 
-        // タイマーをスタート
         let selectionCounter = selectionTime;
         document.getElementById('timer').textContent = selectionCounter;
         timerId = setInterval(() => {
@@ -102,19 +62,13 @@ function startGame() {
                 location.reload();
             }
         }, 1000);
-        canClick = true;  
+
+        canClick = true;
     }, displayTime * 1000);
 }
 
-function playClickSound() {
-    const clickSound = document.getElementById('click-sound');
-    clickSound.currentTime = 0; 
-    clickSound.play();
-}
-
-// タイルが正しいか検証する関数
+// 4. タイル選択: ユーザーがタイルをクリックし、それが正しいかどうかを検証します。
 function verifyTile(index) {
-    // クリック可能かチェック
     if (!canClick) {
         return;
     }
@@ -131,7 +85,6 @@ function verifyTile(index) {
             clearInterval(timerId);
             document.getElementById('next-button').disabled = false;
 
-            // 3回連続で正解した場合、レベルアップフラグをセット
             if (consecutiveCorrect >= 3) {
                 shouldLevelUp = true;
                 consecutiveCorrect = 0;
@@ -142,16 +95,47 @@ function verifyTile(index) {
     }
 }
 
-// ページロード時にメッセージを表示
-document.getElementById('start-message').style.display = 'block';
+// 補助関数群
+function incrementStage() {
+    if (shouldLevelUp) {
+        stage++;
+        document.getElementById('stage').textContent = stage;
+        shouldLevelUp = false;
+    }
+}
 
-let keyPressStarted = false;
+function incrementScore() {
+    score++;
+    document.getElementById('score').textContent = score;
+}
 
-// キーが押されたらゲームスタート
-window.addEventListener('keydown', function (event) {
+function resetScore() {
+    score = 0;
+    document.getElementById('score').textContent = score;
+}
+
+function playClickSound() {
+    const clickSound = document.getElementById('click-sound');
+    clickSound.currentTime = 0;
+    clickSound.play();
+}
+
+// ゲームスタートのシーケンス
+function startGameSequence() {
+    incrementStage();
+    startGame();
+    document.getElementById('next-button').disabled = true;
+}
+
+// キー押下時のゲームスタート
+function startGameOnKeyPress(event) {
     if (!keyPressStarted) {
         startGame();
         document.getElementById('start-message').style.display = 'none';
-        keyPressStarted = true;  
+        keyPressStarted = true;
     }
-});
+}
+
+// ページロード時のメッセージ表示
+document.getElementById('start-message').style.display = 'block';
+let keyPressStarted = false;
